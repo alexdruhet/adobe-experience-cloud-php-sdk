bba611b29c761daf233e42324cc9b63e1052c655
Alex Druhet <alex@listo.studio>
2018-04-13 16:03:58 +0200

    Create very simple and convenient request api

--- src/Adobe/Api/Request.php
+++ src/Adobe/Api/Request.php
@@ -8,38 +8,47 @@
 
 namespace Pixadelic\Adobe\Api;
 
-
 use GuzzleHttp\Client;
 
+/**
+ * Class Request
+ *
+ * @package Pixadelic\Adobe\Api
+ */
 class Request
 {
     protected $method;
     protected $url;
     protected $client;
+    protected $options = [];
 
-    public function __construct($method, $url)
+    /**
+     * Request constructor.
+     *
+     * @param $method
+     * @param $url
+     */
+    public function __construct($method, $url, $body = null)
     {
         $this->method = $method;
         $this->url = $url;
+        if ($body) {
+            if (\is_array($body)) {
+                $this->options['form_params'] = $body;
+            } else {
+                $this->options['body'] = $body;
+            }
+        }
         $this->client = new Client();
-
-
-        $res = $client->request('GET', 'https://api.github.com/repos/guzzle/guzzle');
-        echo $res->getStatusCode();
-// 200
-        echo $res->getHeaderLine('content-type');
-// 'application/json; charset=utf8'
-        echo $res->getBody();
-// '{"id": 1420053, "name": "guzzle", ...}'
 
     }
 
-    public function send() {
-        $res = $this->client->request($this->method, $this->url);
-        echo $res->getStatusCode();
-// 200
-        echo $res->getHeaderLine('content-type');
-// 'application/json; charset=utf8'
-        echo $res->getBody();
+    /**
+     * @return mixed|\Psr\Http\Message\ResponseInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function send()
+    {
+        return $this->client->request($this->method, $this->url, $this->options);
     }
 }
